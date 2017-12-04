ab9aee52efa7411f4a96532906039879f9128850
Alex Seifert <seifert@jochen-schweizer.de>
2017-12-04 14:12:00 +0100

    Only include the CSS routes for non-staging and non-production environments

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var css = require('./routes/css');
+const index = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/css', css);
 
 
 if (app.get('env') === 'staging' || app.get('env') === 'production') {
@@ -33,12 +31,16 @@ if (app.get('env') === 'staging' || app.get('env') === 'production') {
 else {
 	// If not staging or production, just compile the libs.scss
 	require('./lib/compileSass').compileSassLibs().catch(console.error);
+
+	// Also include the CSS routes for on-the-fly compiling
+	const css = require('./routes/css');
+	app.use('/css', css);
 }
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
